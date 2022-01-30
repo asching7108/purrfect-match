54a8f80e2d7ae6df21ab575ba79ae359f584b454
xHoudek <xhoudek@gmail.com>
2022-01-30 17:45:24 -0600

    PM-11: started work on authentication

--- src/routes/LoginPage/LoginPage.js
+++ src/routes/LoginPage/LoginPage.js
@@ -1,10 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./LoginPage.css";
+import AuthService from "../../services/authService";
 
-export default class LoginPage extends Component {  
+export default class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = await AuthService.loginUser({
+      username: this.state.username,
+      password: this.state.password
+    });
+    this.setToken(token);
+  }
+
+  setToken(userToken) {
+    sessionStorage.setItem('token', JSON.stringify(userToken));
+  };
+
+
   render() {
     return (
-      <>
+    <>
+      <div className="loginWrapper">
         <h2>Login</h2>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>
+              <p>Email Address</p>
+              <input type="text" className="form-control" onChange={e => {
+                this.setState({username: e.target.value});
+                }} />
+            </label>
+          </div>
+          <div className="form-group">
+            <label>
+              <p>Password</p>
+              <input type="password" className="form-control" onChange={e => {
+                this.setState({password: e.target.value});
+                }}/>
+            </label>
+          </div>
+          <button className="btn" type="submit">Submit</button>
+        </form>
+      </div>
       </>
     );
   }
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,11 +1,29 @@
 const AuthService = {
+  getToken() {
+    const tokenString = sessionStorage.getItem('token');
+    const userToken = JSON.parse(tokenString);
+    return userToken?.token;
+  },
   isLoggedIn() {
-    // TODO
-    return true;
+    const token = AuthService.getToken();
+
+    // TODO: make sure token is legit / verify with DB
+
+    return token? true : false
   },
   isShelterAdmin() {
     // TODO
     return false;
+  },
+  async loginUser(credentials) {
+    return fetch('http://localhost:8000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json())
   }
 };
 
