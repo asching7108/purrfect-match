8fb114be48d65b70f6e05278553b58135cf4de24
xHoudek <xhoudek@gmail.com>
2022-02-02 08:25:58 -0600

    PM-11: update formatting

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,44 +12,44 @@ import NotFoundPage from '../../routes/NotFoundPage';
 import AuthService from '../../services/authService';
 
 class App extends Component {
-	static contextType = AuthContext;
+  static contextType = AuthContext;
 
-	componentDidMount() {
-		this.context.setLoggedInState(AuthService.isLoggedIn());
-		this.context.setShelterAdminState(AuthService.isShelterAdmin());
-	};
+  componentDidMount() {
+    this.context.setLoggedInState(AuthService.isLoggedIn());
+    this.context.setShelterAdminState(AuthService.isShelterAdmin());
+  };
 
-	render() {
-		return (
-			<div className="App">
-				<header>
-					<Header />
-				</header>
-				<main>
-					<Routes>
-						<Route path={'/'} element={<HomePage />} />
-						<Route
-							path="/login"
-							element={
-								<PublicRoute>
-									<LoginPage />
-								</PublicRoute>
-							}
-						/>
-						<Route
-							path="/favorites"
-							element={
-								<PrivateRoute>
-									<FavoritesPage />
-								</PrivateRoute>
-							}
-						/>
-						<Route path={'*'} element={<NotFoundPage />} />
-					</Routes>
-				</main>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <Header />
+        </header>
+        <main>
+          <Routes>
+            <Route path={'/'} element={<HomePage />} />
+            <Route
+              path="/login"
+              element={
+                <PublicRoute>
+                  <LoginPage />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/favorites"
+              element={
+                <PrivateRoute>
+                  <FavoritesPage />
+                </PrivateRoute>
+              }
+            />
+            <Route path={'*'} element={<NotFoundPage />} />
+          </Routes>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/services/authService.js
+++ src/services/authService.js
@@ -2,43 +2,43 @@ const { isExpired } = require('react-jwt');
 const { HOSTNAME } = require('../config/hostname.config');
 
 const AuthService = {
-	getToken() {
-		const tokenString = localStorage.getItem('token');
+  getToken() {
+    const tokenString = localStorage.getItem('token');
 
     if (tokenString === 'undefined') {
       return null;
     }
 
-		const userToken = tokenString? JSON.parse(tokenString) : null;
-		if (userToken && userToken.token) {
-			return userToken.token;
-		}
-	},
-	isLoggedIn() {
-		const token = AuthService.getToken();
-		if (!token) {
-			return false;
-		}
-		// check JWT expiration
+    const userToken = tokenString ? JSON.parse(tokenString) : null;
+    if (userToken && userToken.token) {
+      return userToken.token;
+    }
+  },
+  isLoggedIn() {
+    const token = AuthService.getToken();
+    if (!token) {
+      return false;
+    }
+    // check JWT expiration
     const tokenIsExpired = isExpired(token);
     if (tokenIsExpired) {
       return false;
     }
     return true;
-	},
-	isShelterAdmin() {
-		// TODO
-		return false;
-	},
-	async loginUser(credentials) {
-		return fetch( HOSTNAME + '/login', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(credentials)
-		}).then((data) => data.json());
-	}
+  },
+  isShelterAdmin() {
+    // TODO
+    return false;
+  },
+  async loginUser(credentials) {
+    return fetch(HOSTNAME + '/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    }).then((data) => data.json());
+  }
 };
 
 export default AuthService;
