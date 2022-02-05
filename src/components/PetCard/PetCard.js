b699a1905d7cf9010e34a27c4f82a05613936112
asching7108 <asching7108@gmail.com>
2022-02-04 22:53:57 -0500

    PM-21 addressed PR comments

--- src/components/PetCard/PetCard.js
+++ src/components/PetCard/PetCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
+import { Link } from 'react-router-dom';
 
 export default function PetCard(props) {
   const { pet } = props;
@@ -9,12 +10,18 @@ export default function PetCard(props) {
     navigate(`/pets/${pet.PetID}`);
   }
 
+  const onClickShelter = (e) => {
+    e.stopPropagation();
+  }
+
   return (
     <div className='col-sm' onClick={onClickCard}>
       <img src={pet.Picture} />
       <h3>{pet.Name}</h3>
       <p>{pet.Sex}, {pet.Age} yrs</p>
-      <p>{pet.ShelterName}</p>
+      <p>
+        <Link to={`/shelters/${pet.ShelterID}`} onClick={onClickShelter}>{pet.ShelterName}</Link>
+      </p>
     </div>
   );
 }
--- src/routes/PetsPage/PetsPage.js
+++ src/routes/PetsPage/PetsPage.js
@@ -5,18 +5,21 @@ import PetsService from '../../services/petsService';
 export default class PetsPage extends Component {
   constructor(props) {
     super(props);
-    this.state = { pets: [] };
+    this.state = {
+      pets: [],
+      petCount: -1  // this is set to prevent the no pets alert before data is loaded
+    };
   }
 
   componentDidMount() {
     PetsService.getPets()
-      .then(pets => this.setState({ pets }))
+      .then(pets => this.setState({ pets, petCount: pets.length }))
       .catch(error => console.log(error));
   }
 
   renderPetRow(rowStart) {
     return (
-      <div className='row'>
+      <div key={rowStart} className='row'>
         {this.state.pets.slice(rowStart, rowStart + 3).map(pet => this.renderPet(pet))}
       </div>
     );
@@ -29,8 +32,19 @@ export default class PetsPage extends Component {
   }
   
   render() {
-    const { pets } = this.state;
-    const petRows = pets.length / 3 + (pets.length % 3 === 0 ? 0 : 1);
+    const { petCount } = this.state;
+
+    // no pets exist
+    if (petCount === 0) {
+      return (
+        <div className="alert alert-success" role="alert">
+          More pets will be available soon! Check back later :)
+        </div>
+      );
+    }
+
+    // place pets in a 3-cols grid
+    const petRows = Math.floor(petCount / 3) + (petCount % 3 === 0 ? 0 : 1);
     return (
       <div className='container'>
         {[...Array(petRows)].map((_, i) => this.renderPetRow(i * 3))}
