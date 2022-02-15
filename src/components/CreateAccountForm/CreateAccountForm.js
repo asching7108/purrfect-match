3e42def3b96131c4dd8ff6fc17bc763f24525123
xHoudek <xhoudek@gmail.com>
2022-02-15 16:16:16 -0600

    PM-24: dynamically add US State options

--- src/components/CreateAccountForm/CreateAccountForm.js
+++ src/components/CreateAccountForm/CreateAccountForm.js
@@ -4,6 +4,7 @@ import AuthContext from '../../context/AuthContext';
 import UsersService from '../../services/usersService';
 import SheltersService from '../../services/sheltersService';
 import PasswordChecklist from 'react-password-checklist'
+import USStates from './USStates.json'
 
 export default class CreateAccountForm extends Component {
   static contextType = AuthContext;
@@ -123,14 +124,14 @@ export default class CreateAccountForm extends Component {
   }
 
   renderUSStates() {
+    let stateOptions = [<options></options>];
+    const states = USStates.states;
+    for (let i = 0; i < states.length; i++) {
+      stateOptions.push(<option>{states[i]}</option>);
+    }
     return (
       <>
-        <option></option><option>AL</option><option>AK</option><option>AZ</option><option>AK</option><option>CA</option><option>CO</option><option>CT</option><option>DC</option>
-        <option>DE</option><option>FL</option><option>GA</option><option>HI</option><option>ID</option><option>IL</option><option>IN</option><option>IA</option><option>KS</option>
-        <option>KY</option><option>LA</option><option>ME</option><option>MD</option><option>MA</option><option>MI</option><option>MN</option><option>MS</option><option>MO</option>
-        <option>MT</option><option>NE</option><option>NV</option><option>NH</option><option>NJ</option><option>NM</option><option>NY</option><option>NC</option><option>ND</option>
-        <option>OH</option><option>OK</option><option>OR</option><option>PA</option><option>PR</option><option>RI</option><option>SC</option><option>SD</option><option>TN</option>
-        <option>TX</option><option>UT</option><option>VT</option><option>VA</option><option>WA</option><option>WV</option><option>WI</option><option>WY</option>
+        {stateOptions}
       </>
     )
   }
