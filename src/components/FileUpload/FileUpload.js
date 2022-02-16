680715aecf647463a5643ca1753d7d7db8552116
asching7108 <asching7108@gmail.com>
2022-02-16 21:05:22 +0800

    fixed pet form issues

--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -1,12 +1,13 @@
 //reference: https://www.positronx.io/react-file-upload-tutorial-with-node-express-and-multer/
 import React, { Component } from 'react';
 import { FormGroup } from '../Utils/Utils';
+
 export default class FilesUploadComponent extends Component {
   render() {
     return (
-            <div className="form-group">
-              <input type="file" name="uploaded_file" onChange={this.props.onChange} />
-            </div>
+      <FormGroup>
+        <input type="file" name="uploaded_file" {...this.props} />
+      </FormGroup>
     )
   }
 }
