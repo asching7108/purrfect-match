d80a2131e0886a4c7d4f4659a31959c15b3c9631
asching7108 <asching7108@gmail.com>
2022-02-18 10:45:40 +0800

    PM-37 reverted changes

--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -1,10 +1,12 @@
 //reference: https://www.positronx.io/react-file-upload-tutorial-with-node-express-and-multer/
 import React, { Component } from 'react';
-
+import { FormGroup } from '../Utils/Utils';
 export default class FilesUploadComponent extends Component {
   render() {
     return (
-      <input className="form-control" type="file" name="uploaded_file" {...this.props} />
+            <div className="form-group">
+              <input type="file" name="uploaded_file" onChange={this.props.onChange} />
+            </div>
     )
   }
 }
